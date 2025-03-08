f50c71f306172c637ccecc94fda30f6d45677e66
Gopi-Git001 <gopi.thungam.001@gmail.com>
2025-03-08 08:32:46 -0600

    Some changes on viewing pages

--- Blog_clone_project/Blog_clone_project/settings.py
+++ Blog_clone_project/Blog_clone_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -115,8 +115,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
+# mysite/settings.py
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+# Add these new configurations
+STATICFILES_DIRS = [
+    BASE_DIR / 'mysite' / 'static',  # Direct path to your static folder
+]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # For collectstatic command
 
 LOGIN_REDIRECT_URL = '/'
 
